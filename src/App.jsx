ddbcc28b42411a6490b60b345d78303cde243810
Musakalamz <ogunsolumusa@gmail.com>
2024-12-17 18:13:51 +0100

    Setting up page navbar using NAVLINK which auto indicates the actuve tab when clicked

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import About from "./pages/About";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
+import PageNav from "./components/PageNav";
 
 function App() {
   return (
     <BrowserRouter>
+      <PageNav />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="about" element={<About />} />
