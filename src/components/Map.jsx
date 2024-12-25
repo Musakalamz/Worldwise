1e74ff7c71854fbfc7ee8fe2bbda78654aa7d61a
Musakalamz <ogunsolumusa@gmail.com>
2024-12-25 11:48:20 +0100

    Installing Leaflet library and putting it map on the screen

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,13 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState } from "react";
+
 function Map() {
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState([9.082, 8.6753]);
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
@@ -14,10 +18,22 @@ function Map() {
         navigate("form");
       }}
     >
-      <h1>Map</h1>
-      <h2>
-        Latitude position is {lat} and Longitude position is {lng}
-      </h2>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
