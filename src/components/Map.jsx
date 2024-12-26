536cbdfbef5a7d188a6a06db47fc4abeacd2b61d
Musakalamz <ogunsolumusa@gmail.com>
2024-12-26 10:21:31 +0100

    Fetching city data in the form, adding a new city and deleting a new city

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,31 +1,29 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
-import styles from "./Map.module.css";
-
 import {
   MapContainer,
   TileLayer,
   Marker,
   Popup,
   useMap,
-  useMapEvent,
+  useMapEvents,
 } from "react-leaflet";
+
+import styles from "./Map.module.css";
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import { useGeolocation } from "../hooks/useGeolocation";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 import Button from "./Button";
 
 function Map() {
   const { cities } = useCities();
-  const [searchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
-
-  const [mapPosition, setMapPosition] = useState([9.082, 8.6753]);
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(
     function () {
@@ -46,9 +44,10 @@ function Map() {
     <div className={styles.mapContainer}>
       {!geolocationPosition && (
         <Button type="position" onClick={getPosition}>
-          {isLoadingPosition ? "Loading..." : "use your position"}
+          {isLoadingPosition ? "Loading..." : "Use your position"}
         </Button>
       )}
+
       <MapContainer
         center={mapPosition}
         zoom={6}
@@ -56,8 +55,8 @@ function Map() {
         className={styles.map}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
         {cities.map((city) => (
           <Marker
@@ -69,27 +68,25 @@ function Map() {
             </Popup>
           </Marker>
         ))}
-        <ChangeView position={mapPosition} />
+
+        <ChangeCenter position={mapPosition} />
         <DetectClick />
       </MapContainer>
     </div>
   );
 }
 
-function ChangeView({ position }) {
+function ChangeCenter({ position }) {
   const map = useMap();
   map.setView(position);
-  return <></>;
+  return null;
 }
 
 function DetectClick() {
   const navigate = useNavigate();
 
-  useMapEvent({
-    click: (e) => {
-      console.log(e);
-      navigate(`form?${e.latlng.lat}&${e.latlng.lng}`);
-    },
+  useMapEvents({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
   });
 }
 
